094e5118a0d31ff08a3d6088791ba517e39e5c4f
Braian <braianvoficial@gmail.com>
2022-09-15 23:18:33 -0300

    Finishing class number four

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,12 +1,84 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Background } from '../../components/Background';
 
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native';
+
+import { Entypo } from '@expo/vector-icons';
+import logo from '../../assets/logo-nlw-esports.png';
+
 import { styles } from './styles';
+import { THEME } from '../../theme';
+import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { useEffect, useState } from 'react';
+
+interface RouteParms {
+  id: string;
+  title: string;
+  banneUrk: string
+}
 
 export function Game() {
+  const route = useRoute();
+  const game = route.params as RouteParms;
+  const navigation = useNavigation();
+  const [ads, setAds] = useState<DuoCardProps[]>([]);
+
+  useEffect(() => {
+    fetch(`http://192.168.1.228:3333/games/${game.id}/ads`)
+    .then(res => res.json())
+    .then(data => {
+      setAds(data);
+    })
+  }, [])
+
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
+
   return (
     <Background>
         <SafeAreaView style={styles.container}>
+
+          <View style={styles.header}>
+            <TouchableOpacity onPress={handleNavigateBack}>
+              <Entypo 
+                name='chevron-thin-left'
+                color={THEME.COLORS.CAPTION_300}
+                size={20}
+              />
+            </TouchableOpacity>
+
+            <Image source={logo} style={styles.logo} />
+            <View style={styles.right} />
+          </View>
+
+          <Image 
+            source={{ uri: game.banneUrk }}
+            style={styles.cover}
+            resizeMode="cover"
+          />
+
+          <Heading 
+            title={game.title}
+            subtitle="Conenct-se e começe a jogar!"
+          />
+
+          <FlatList 
+            data={ads}
+            keyExtractor={item => item.id}
+            horizontal={true}
+            style={styles.containerList}
+            contentContainerStyle={ads.length === 0 ? styles.emptyListContent : styles.contentList }
+            showsHorizontalScrollIndicator={false}
+            ListEmptyComponent={() => (
+              <Text style={styles.emptyText}>Não há anúncios para este jogo ainda</Text>
+            )}
+            renderItem={({item}) => (
+              <DuoCard data={item} onConnect={() => {}} />
+            )}
+          />
 
         </SafeAreaView>
     </Background>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -15,12 +15,12 @@ export function Home() {
 
   const navigation = useNavigation();
 
-  function handleOpenGame() {
-    navigation.navigate('game');
+  function handleOpenGame({ id, title, banneUrk }: GameCardProps) {
+    navigation.navigate('game', { id, title, banneUrk });
   }
 
   useEffect(() => {
-    fetch('http://192.168.1.115:3333/games')
+    fetch('http://192.168.1.228:3333/games')
     .then(res => res.json())
     .then(data => {
       setGames(data);
@@ -45,10 +45,9 @@ export function Home() {
               horizontal={true}
               showsHorizontalScrollIndicator={false}
               renderItem={({item}) => (
-                  <GameCard data={item} onPress={handleOpenGame} />
+                  <GameCard data={item} onPress={() => handleOpenGame(item)} />
               )}
           />
-
           
       </SafeAreaView>
     </Background>
